e7f81f3bc5053034ad2ffb23c697248e02f23a4f
Salvatore Fara <sf.salvatorefara@gmail.com>
2021-02-11 12:18:48 +0000

    created a jupyter notebook

---
+++ models.py
@@ -0,0 +1,117 @@
+from utils import *
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def conv_model(nlayers=10, nchannels=10, Tx=None, Trej=0, preemphasis_weights=[-.95,1], print_summary=False, learning_rate=.001):
+
+    # Single convolutional layer
+    def conv_block(X,k,dilation):
+        Z = layers.Conv1D(filters = nchannels,
+                          kernel_size = 2,
+                          padding = 'causal',
+                          dilation_rate = dilation,
+                          activation = 'relu',
+                          name = 'Z'+str(k))(X)
+        Zmix = layers.Conv1D(1,1,name = 'Z'+str(k)+'_mix')(Z)
+        X = layers.Add(name = 'X'+str(k+1))([X,Zmix])
+        return X, Z
+
+    # Input layer
+    X_input = layers.Input(shape = (Tx,1), name = 'X0')
+    X = X_input
+
+    # Hidden layers - stacked dilated convolutional layers
+    Zlist = []
+    for k in range(nlayers):
+        X, Z = conv_block(X,k,dilation=2**k)
+        Zlist.append(Z)
+
+    # Output layer
+    Z = layers.Concatenate(axis=2, name='Z')(Zlist)
+    Y = layers.Conv1D(1,1,name = 'Y')(Z)
+
+    # Create model
+    model = keras.Model(inputs=[X_input], outputs=Y)
+    if print_summary:
+        model.summary()
+
+    # Specify the preemphasis filter (used in the loss function)
+    if preemphasis_weights is not None:
+        Preemphasis = tf.constant(preemphasis_weights, 
+                                  shape=[len(preemphasis_weights),1,1], 
+                                  dtype=tf.float32)
+
+    # Compile model
+    opt = tf.keras.optimizers.Adam(learning_rate)
+    model.compile(optimizer=opt, loss=esr_dc(Trej, Preemphasis), metrics=esr)
+    
+    return model
+
+def recurrent_model(units=32, celltype='GRU', Tx=None, Trej=0, preemphasis=True, preemphasis_weights=[-.95,1], print_summary=False, learning_rate=.001):
+    '''
+    Recurrent neural network model with or without pre-emphasis filter. 
+    
+    '''
+    
+    # Xavier normal inizializer for the weights
+    initializer = keras.initializers.GlorotNormal()
+
+    # specify recurrent cell type
+    if celltype=='GRU':
+        Rcell = layers.GRU(units=units, return_sequences=True, kernel_initializer=initializer)
+    elif celltype=='LSTM':
+        Rcell = layers.LSTM(units=units, return_sequences=True, kernel_initializer=initializer)
+    elif celltype=='RNN':
+        Rcell = layers.SimpleRNN(units=units, return_sequences=True, kernel_initializer=initializer)
+    
+    # specify preemphasis filter 
+    if preemphasis:
+        Preemphasis = layers.Conv1D(1,2,padding='valid',use_bias=False,trainable=False, \
+                                   name = 'preemphasis')
+        Preemphasis.build((1,))
+        Preemphasis.set_weights(np.array(preemphasis_weights).reshape((1,2,1,1)))
+    
+    # Base model
+    Xres = layers.Input(shape=(Tx,1))
+    X = Rcell(Xres)
+    X = layers.Dense(1,activation='linear', kernel_initializer=initializer)(X)
+    Y = layers.Add()([X,Xres])
+    
+    # Set the output of the model:
+    # - during training (Trej>0), add a cropping layer to remove model trainsients
+    # - add preemphasis filter if needed 
+    if Trej==0 and not preemphasis:
+        outputs = Y
+    elif Trej==0 and preemphasis:
+        outputs = Preemphasis(Y)
+    elif Trej and not preemphasis:
+        outputs = layers.Cropping1D((Trej,0))(Y)
+    elif Trej and preemphasis:
+        out = layers.Cropping1D((Trej,0), name = 'dc')(Y)
+        
+        # crop less to compensate for the "valid" convolution of the pre-
+        # emphasis filter
+        Ype = layers.Cropping1D((Trej-1,0))(Y)
+        out_pe = Preemphasis(Ype)
+        
+        # return two outputs: after (out_pe) and before (out) the pre-emphasis
+        # filter
+        outputs = [out_pe, out]
+    
+    # create the model
+    model = keras.Model(inputs=[Xres], outputs=outputs)
+    if print_summary:
+        print(model.summary())
+        
+    # compile model
+    opt = tf.keras.optimizers.Adam(learning_rate,clipvalue=10)
+    if Trej and preemphasis:
+        loss = [esr,dc]
+        metrics = [esr,dc]
+    else:
+        loss = esr
+        metrics = esr
+    model.compile(optimizer=opt, loss=loss, metrics=metrics)
+    
+    return model
--- utils.py
+++ utils.py
@@ -4,6 +4,28 @@ import soundfile as sf
 import simpleaudio as sa
 import time
 import tensorflow.keras.backend as kb
+import tensorflow as tf
+from IPython.display import display, Audio
+
+def round_nextpow2(v):
+    '''
+    Round up to the next highest power of 2. 
+    Source:
+    https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
+    
+    '''
+    
+    v = int(v)
+    v -= 1
+    v |= v >> 1
+    v |= v >> 2
+    v |= v >> 4
+    v |= v >> 8
+    v |= v >> 16
+    v += 1
+    v += v==0
+    
+    return v
 
 def load_wavedata(data_dir):
     """
@@ -26,7 +48,7 @@ def load_wavedata(data_dir):
     # Retreive input and output file names
     file_type = np.array([x.split('-')[0] for x in file_names])
     input_names = file_names[file_type == 'input']
-    output_names = file_names[file_type != 'input']#
+    output_names = file_names[file_type != 'input']
     n_inputs = len(input_names)
     
     # Load data into a dictionary (parse amp types and specs)
@@ -65,6 +87,44 @@ def load_wavedata(data_dir):
     
     return wavedata
 
+def data_chunk(X, Tx, Trej = 0):
+    '''
+    
+    Chunks X into examples of length Tx, with overlap Trej. 
+    
+    Parameters
+    ----------
+    X : numpy.ndarray
+        Data to be chunked, with shape = (examples,time)
+    Tx: int
+        Chunk size
+    Trej: int, optional
+        Chunk overlap; default is 0
+
+    Returns
+    -------
+    Xchunk : numpy.ndarray
+        Chunked array.
+
+    '''
+    
+    Ttot = np.shape(X)[1]
+    n_examples = int((Ttot-Tx)/(Tx-Trej)+1)
+    n_inputs = np.shape(X)[0]
+
+    Xchunk = np.zeros((n_examples*np.shape(X)[0],Tx))
+    for k in range(n_examples):
+        example_start = k*(Tx-Trej)
+        example_end = k*(Tx-Trej) + Tx
+        Xchunk[k*n_inputs:(k+1)*n_inputs,:] = X[:,example_start:example_end]
+
+    # just a test to make sure the data examples overlap in the region to reject
+    k = int(np.shape(X)[0]/2)
+    if Trej:
+        assert(all(Xchunk[k,-Trej:]==Xchunk[k+n_inputs,:Trej]))
+        
+    return Xchunk
+
 def audio_playback(audio_data, samplerate = 44100, max_playback_time = 2):
     '''
     
@@ -99,6 +159,54 @@ def audio_playback(audio_data, samplerate = 44100, max_playback_time = 2):
     time.sleep(max_playback_time)
     play_obj.stop()
     
+def audio_playback_gui(audio_data, samplerate = 44100, file_name = 'audio_example_0', audio_example_folder = 'audio_examples'):
+    '''
+    Improved version of audio_playback() based on IPython; it integrates well
+    with jupyter notebooks. 
+    
+    Parameters
+    ----------
+    audio_data : str or numpy.ndarray
+        Either a file name or a numpy array containing wave data 
+    samplerate : int, optional
+        The default is 44100.
+        Unused if audio_data is string.
+    file_name : str, optional
+        The default is 'audio_example_0'.
+        Unused if audio_data is string.
+    audio_example_path: str, optional
+        The default is 'audio_examples'.
+        Unused if audio_data is string.
+
+    Returns
+    -------
+    None.
+
+    '''
+
+    # if audio_data is a numpy array, create a corresponding audio file
+    if isinstance(audio_data, np.ndarray):
+
+        # create a folder to store the audio examples (if it does not exist yet)
+        if not os.path.exists(audio_example_folder):
+            os.makedirs(audio_example_folder)
+
+        # make sure audio data has shape = frames*channels
+        if len(np.shape(audio_data))>1:
+            # here we assume nframes>2 
+            if np.shape(audio_data)[0] < np.shape(audio_data)[1]:
+                audio_data = audio_data.T
+
+        # create audio file
+        file_path = audio_example_folder + '/' + file_name + '.wav'
+        sf.write(file_path,audio_data,samplerate)
+
+    else:
+        file_path = audio_data
+
+    # play audio
+    display(Audio(file_path))
+    
 def InOut_plot(ax1,X,Y):
     color = 'tab:blue'
     ax1.plot(X, color = color)
@@ -108,21 +216,48 @@ def InOut_plot(ax1,X,Y):
     ax2 = ax1.twinx()
     ax2.plot(Y,color = color)
     ax2.tick_params(axis='y', labelcolor=color)
-    
-# def esr_dc(gamma = 1):
-#     def loss(y_true,y_pred):
-#         ypow = kb.mean(kb.square(y_true))
-#         esr = kb.mean(kb.square(y_true - y_pred))/ypow
-#         dc = kb.square(kb.mean(y_true - y_pred))/ypow
-#         return gamma*esr+(1-gamma)*dc
-#     return loss
 
 def dc(y_true, y_pred):
+    '''
+    
+    DC loss.
+    
+    '''
+    
     ypow = kb.mean(kb.square(y_true))
-    return kb.square(kb.mean(y_true - y_pred))/(ypow+1e-5)
-
+    return kb.square(kb.mean(y_true - y_pred))/(ypow+1e-10)
 
 def esr(y_true,y_pred):
+    '''
+    
+    Error-to-signal ratio (esr).
+    
+    '''
+    
     ypow = kb.mean(kb.square(y_true))
-    return kb.mean(kb.square(y_true - y_pred))/(ypow+1e-5)
-   
+    return kb.mean(kb.square(y_true - y_pred))/(ypow+1e-10)
+
+def esr_dc(Trej=None, preemphasis=None):
+    '''
+    
+    Error-to-signal ratio (esr) + DC loss.
+    
+    '''
+    
+    def loss(y_true,y_pred):
+        # Reject model transients
+        if Trej is not None:
+            y_true = y_true[:,Trej:,:]
+            y_pred = y_pred[:,Trej:,:]
+        
+        # Compute dc loss
+        dc_loss = dc(y_true, y_pred)
+        
+        # Apply pre-emphasis filter
+        if preemphasis is not None:
+            y_true = tf.nn.conv1d(y_true, preemphasis, stride=1, padding='SAME')
+            y_pred = tf.nn.conv1d(y_pred, preemphasis, stride=1, padding='SAME')
+        
+        ypow = kb.mean(kb.square(y_true))
+        return kb.mean(kb.square(y_true - y_pred))/(ypow+1e-10) + dc_loss
+    return loss
